f249492afa84cd745bb554a35d3a6ba984f2041f
electricplant <deanagerhard@gmail.com>
2024-09-23 19:43:03 +0200

    take the cash and get outta here

--- Projects/layer 2/pipex/Working/inc/include.h
+++ Projects/layer 2/pipex/Working/inc/include.h
@@ -6,7 +6,7 @@
 /*   By: dgerhard <dgerhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:33:27 by dgerhard          #+#    #+#             */
-/*   Updated: 2024/09/23 15:00:52 by dgerhard         ###   ########.fr       */
+/*   Updated: 2024/09/23 19:30:21 by dgerhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,21 @@
 
 # include "../ft_printf/include/libft.h"
 # include "../ft_printf/include/reqs.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>  
+# include <stdlib.h>
 
-int	main(void);
-
+int		main(int ac, char **av, char **env);
+int		open_file(char *file, int n);
+char	*my_getenv(char *name, char **env);
+char	*get_path(char *cmd, char **env);
+void	execute(char *cmd, char **env);
+void	ft_free_tab(char **tab);
+void	ft_error(int n_exit);
 
 #endif
