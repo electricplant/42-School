ad89b6602b215ed8b6559a73ed1c2635d79c3cba
Dean42 <deanagerhard@gmail.com>
2024-09-24 15:54:15 +0200

    finished level 1 prep

---
+++ Projects/exam_02prep/Level 1/rotone.c
@@ -0,0 +1,49 @@
+// Assignment name  : rotone
+// Expected files   : rotone.c
+// Allowed functions: write
+// --------------------------------------------------------------------------------
+
+// Write a program that takes a string and displays it, replacing each of its
+// letters by the next one in alphabetical order.
+
+// 'z' becomes 'a' and 'Z' becomes 'A'. Case remains unaffected.
+
+// The output will be followed by a \n.
+
+// If the number of arguments is not 1, the program displays \n.
+
+// Example:
+
+// $>./rotone "abc"
+// bcd
+// $>./rotone "Les stagiaires du staff ne sentent pas toujours tres bon." | cat -e
+// Mft tubhjbjsft ev tubgg of tfoufou qbt upvkpvst usft cpo.$
+// $>./rotone "AkjhZ zLKIJz , 23y " | cat -e
+// BlkiA aMLJKa , 23z $
+// $>./rotone | cat -e
+// $
+// $>
+// $>./rotone "" | cat -e
+// $
+// $>
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i])
+		{
+		if ((argv[1][i] >= 'a') && (argv[1][i] <= 'z'))
+			argv[1][i] = (argv[1][i] - 'a' + 1) % 26 + 'a';
+		else if ((argv[1][i] >= 'A') && (argv[1][i]))
+			argv[1][i] = (argv[1][i] - 'A' + 1) % 26 + 'A';
+		write(1, &argv[1][i], 1);
+		i++;
+		}
+	}
+	write(1, "\n", 1);
+}
