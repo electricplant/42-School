2e80443a6b763d07c4a459486201acf3a19565e4
Dean42 <deanagerhard@gmail.com>
2024-09-24 16:02:57 +0200

    msgsgsgsgsgs

--- Projects/exam_02prep/Level 2/alpha_mirror.c
+++ Projects/exam_02prep/Level 2/alpha_mirror.c
@@ -36,9 +36,9 @@ int	main(int argc, char **argv)
 	while (argv[1][i] && (argc == 2))
 	{
 		if ((argv[1][i] >= 'a') && (argv[1][i] <= 'z'))
-			argv[1][i] = (argv[1][i] + 'a' - 25) % 25 + 'a';
+			argv[1][i] = (argv[1][i] - 'a' + 25)% 26 + 'a';
 		else if ((argv[1][i] >= 'A') && (argv[1][i] <= 'Z'))
-			argv[1][i] = (argv[1][i] - 'A' + 26) % 26 + 'A';
+			argv[1][i] = (argv[1][i] - 'A' + 13)% 26 + 'A';
 		write(1, &argv[1][i], 1);
 		i++;
 	}
