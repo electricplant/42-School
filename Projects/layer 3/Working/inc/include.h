29096b0a7145d3f0d8611dc3cc82f0eed1c02cdf
electricplant <deanagerhard@gmail.com>
2024-10-13 11:26:00 +0200

    fixed up printf

---
+++ Projects/layer 3/Working/inc/include.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDE_H
+# define INCLUDE_H
+
+
+
+
+# endif
---
+++ Projects/layer 3/Working/src/ft_printf/src/ft_printfutils.c
@@ -0,0 +1,50 @@
+#include "reqs.h"
+#include "libft.h"
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	ft_printstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str == NULL)
+	{
+		ft_putstr("(null)");
+		return (6);
+	}
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (i);
+}
+
+int ft_printnumber(int n)
+{
+	int		len;
+	char	*num;
+
+	len = 0;
+	num = ft_itoa(n);
+	len = ft_printstr(num);
+	free(num);
+	return (len);
+}
+
+int	ft_print_perc(void)
+{
+	write(1, "%", 1);
+	return (1);
+}
