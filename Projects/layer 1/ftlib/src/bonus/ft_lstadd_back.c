25d37ad21e13873cc8077c1617d62d3cd0f60b09
Dean42 <deanagerhard@gmail.com>
2024-12-09 11:54:04 +0100

    hlo

---
+++ Projects/layer 1/ftlib/src/bonus/ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgerhard <dgerhard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 13:31:19 by dgerhard          #+#    #+#             */
+/*   Updated: 2024/10/02 15:02:10 by dgerhard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	if (*lst)
+		ft_lstlast(*lst)->next = new;
+	else
+		*lst = new;
+}
