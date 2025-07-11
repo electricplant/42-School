5beeb51f93dddf09687ed34c358c1af88ab3e28e
electricplant <dgerhard@student.42.fr>
2025-07-11 12:58:05 +0200

    hi

--- Projects/layer 4/cpp_module_1/Working/ex00/randomChump.cpp
+++ Projects/layer 4/cpp_module_1/Working/ex00/randomChump.cpp
@@ -6,18 +6,15 @@
 /*   By: dgerhard <dgerhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 11:53:43 by dgerhard          #+#    #+#             */
-/*   Updated: 2025/04/19 20:28:16 by dgerhard         ###   ########.fr       */
+/*   Updated: 2025/07/11 12:47:28 by dgerhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-void	randomChump(std::string name)
+void Zombie::randomChump(std::string name)
 {
-	Zombie* zombie = newZombie(name);
-
-	zombie->announce();
-
-	std::cout << "Destroyed : " << name << std::endl;
-	delete zombie;
+	Zombie Zombie(name);
+	Zombie.announce();
+	return;
 }
--- Projects/layer 4/cpp_module_1/Working/ex01/Zombie.cpp
+++ Projects/layer 4/cpp_module_1/Working/ex01/Zombie.cpp
@@ -6,18 +6,28 @@
 /*   By: dgerhard <dgerhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 19:10:23 by dgerhard          #+#    #+#             */
-/*   Updated: 2025/05/03 13:34:16 by dgerhard         ###   ########.fr       */
+/*   Updated: 2025/07/11 12:57:19 by dgerhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-void Zombie::announce(void)
+Zombie::Zombie()
 {
-	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << "Zombie constructor called" << std::endl;
 }
 
-void Zombie::setName(std::string zombieName)
+void	Zombie::announce()
 {
-	name = zombieName;
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void	Zombie::setName(std::string name)
+{
+	this->name = name;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << "Destroyed : " << this->name << std::endl;
 }
