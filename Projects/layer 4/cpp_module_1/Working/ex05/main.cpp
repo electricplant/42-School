2234ef506793dcc8eb0d6334b33e7a2d0fa52bdf
electricplant <dgerhard@student.42.fr>
2025-07-14 13:35:52 +0200

    hoop

--- Projects/layer 4/cpp_module_1/Working/ex05/main.cpp
+++ Projects/layer 4/cpp_module_1/Working/ex05/main.cpp
@@ -6,17 +6,20 @@
 /*   By: dgerhard <dgerhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 10:16:10 by dgerhard          #+#    #+#             */
-/*   Updated: 2025/04/28 10:37:15 by dgerhard         ###   ########.fr       */
+/*   Updated: 2025/07/14 13:19:48 by dgerhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
 	Harl Harl2;
 
-	Harl2.complain("INFO");
+	if (argc == 2)
+	{
+		Harl2.complain(argv[1]);
+	}
+	// Harl2.complain("INFO");
 
-	
 }
