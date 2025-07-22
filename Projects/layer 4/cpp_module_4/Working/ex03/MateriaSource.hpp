8bec9b9d78c6929fd1503c0f239890b824112a68
electricplant <dgerhard@student.42.fr>
2025-07-22 12:29:45 +0200

    ere

--- Projects/layer 4/cpp_module_4/Working/ex03/MateriaSource.hpp
+++ Projects/layer 4/cpp_module_4/Working/ex03/MateriaSource.hpp
@@ -6,18 +6,21 @@
 /*   By: dgerhard <dgerhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 09:35:29 by dgerhard          #+#    #+#             */
-/*   Updated: 2025/07/22 12:21:29 by dgerhard         ###   ########.fr       */
+/*   Updated: 2025/07/22 12:28:57 by dgerhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-class AMateria;
+#include <iostream>
+#include <string>
+
+#include "AMateria.hpp"
 
 class IMateriaSource
 {
 	public:
 		virtual ~IMateriaSource() {}
-		virtual void learnMateria(AMateria*) = 0;
+		virtual void learnMateria(AMateria *materia) = 0;
 		virtual AMateria* createMateria(std::string const& type) = 0;
 };
