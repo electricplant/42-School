ca8e8d4ffd30674d03a9f80c162767f10ef2ef02
electricplant <deanagerhard@gmail.com>
2025-11-12 07:22:45 +0100

    cpp07 checked

--- Projects/layer 5/cpp 5-9/cpp07/Working/ex01/iter.hpp
+++ Projects/layer 5/cpp 5-9/cpp07/Working/ex01/iter.hpp
@@ -6,13 +6,12 @@
 /*   By: dgerhard <dgerhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 12:25:33 by dgerhard          #+#    #+#             */
-/*   Updated: 2025/10/24 13:38:23 by dgerhard         ###   ########.fr       */
+/*   Updated: 2025/11/12 07:18:24 by dgerhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include "iter.hpp"
 #include <cstddef>
 
 template <typename Toop, typename Func>
